2be6903751b2a245cd1ff584a95274a972ec2916
Tanvir <tntanvir2382018@gmail.com>
2023-12-07 23:13:28 +0600

    add al-quran mp3

---
+++ src/component/Alquran/MusicPlayer.jsx
@@ -0,0 +1,49 @@
+// MusicPlayer.js
+import React, { useState, useRef } from 'react';
+import { FaPause, FaPlay } from 'react-icons/fa';
+
+const MusicPlayer = ({ songs }) => {
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playPauseHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    // Do something with the current time and duration if needed
+  };
+
+  const songEndHandler = () => {
+    // Logic for what to do when the song ends
+    // For example, play the next song
+    setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+  };
+
+  return (
+    <div className="music-player">
+      {/* <h2>{songs[currentSongIndex].title}</h2> */}
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onEnded={songEndHandler}
+        ref={audioRef}
+        src={songs}
+      ></audio>
+      <div className="controls">
+        <button onClick={playPauseHandler}>
+          {isPlaying ? <FaPause /> : <FaPlay />}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MusicPlayer;
--- src/component/Alquran/Sura.jsx
+++ src/component/Alquran/Sura.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { FaCopy } from "react-icons/fa";
 import { Spinner } from '@material-tailwind/react';
+import MusicPlayer from './MusicPlayer';
 
 
 const Sura = () => {
@@ -42,11 +43,12 @@ const Sura = () => {
                         </div>
                         <div className=' gap-7 flex flex-col p-4'>
                             {
-                                sura.ayahs && sura.ayahs.map(({ text, numberInSurah }) => (
+                                sura.ayahs && sura.ayahs.map(({ text, numberInSurah, audio }) => (
                                     <div key={numberInSurah} className='bg-[#27272a] rounded-md py-5 px-2 min-h-[10rem]  md:flex justify-between cursor-pointer '>
                                         <div className='flex items-center md:flex-col w-16 xs:bg-red-900 pb-6 gap-5'>
                                             <h1>{numberInSurah}</h1>
                                             <h1 className='hover:text-[#1d4ed8] duration-150' onClick={() => copyToClipboard(text)}><FaCopy /></h1>
+                                            <MusicPlayer songs={audio} />
                                         </div>
                                         <div className='pr-5 md:w-11/12'>
                                             <h1 className='text-end text-4xl  duration-300'>{text}</h1>
