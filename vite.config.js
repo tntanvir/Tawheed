99e3f1f1258d7c6d2df9e45a26b2b7f513963de5
Tanvir <tntanvir2382018@gmail.com>
2023-12-30 22:08:25 +0600

    PWS

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,47 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
-
 const manifestForPlugIn = {
-  registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "maskable_icon.png"],
+  registerType: 'prompt',
+  includeAssests: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
   manifest: {
-    name: "TAWHEED",
-    short_name: "TAWHEED",
-    description: "Digital home for all things Muslim",
-    icons: [
-      {
-        src: "android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "maskable_icon.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
+    name: "React-vite-app",
+    short_name: "react-vite-app",
+    description: "I am a simple vite app",
+    icons: [{
+      src: '/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png',
+      purpose: 'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable',
+    }
     ],
-    // theme_color: "#209CEE",
-    background_color: "#ffffff",
+    theme_color: '#171717',
+    background_color: '#f0e7db',
     display: "standalone",
-    scope: "/",
+    scope: '/',
     start_url: "/",
-    orientation: "portrait",
-  },
-};
-
+    orientation: 'portrait'
+  }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react(), VitePWA(manifestForPlugIn)],
-});
+})
